21ecced34be655fdc3a9cc79baceebb09c928f4a
pikanotjx <kwokjingxuan@gmail.com>
2023-02-06 04:13:57 +0800

    added countdown timer

--- js/game.js
+++ js/game.js
@@ -2,11 +2,23 @@ $(document).ready(function(){
     $(".logo").hide();
     $("#game").hide();
     $("#game-over").hide();
-    $("#score").hide();
+    $("#countdown").hide();
     $("#start").on("click", function(){
         $("#start").hide();
-        $("#game").show();
-        $("#score").show();
-        $(".logo").show();
+        $("#countdown").show();
+        var countdown = 4;
+        var countdownTimer = setInterval(function(){
+            if(countdown <= 0){
+                clearInterval(countdownTimer);
+                $("#countdown").hide();
+                $("#game").show();
+                $(".logo").show();
+                startGame();
+            } else{
+                $("#countdown-timer").html(countdown);
+                countdown--;
+            }
+        }, 1000);
+
     });
 });

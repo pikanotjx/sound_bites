363d2f3043ad337b3d4bf319d970c14c08a3a770
pikanotjx <kwokjingxuan@gmail.com>
2023-02-10 04:00:57 +0800

    game handle and database handle

--- js/game.js
+++ js/game.js
@@ -1,163 +1,162 @@
 var artist = localStorage.getItem("artist");
 
-
 const optionContainer = $(".option-container");
-let songCounter = 0;
+var songCounter = 0;
+let correctAnswers = 0;
 let currentSong;
-let availableSongs = [];
 let songList = [];
-let allOptions = [];
-let availableOptions = [];
+let allSongs = [];
+var startTime;
+var endTime;
+var dbDown = false;
 
 $(document).ready(function () {
-    $("#artist-name").html(artist);
-    setAvailableSongs();
-    getNewSong();
+    $("#artist-name").html(artist.split("_").join(" "));
+    setAvailableSongs()
+        .then(() => {
+            $("#start-game").on("click", function () {
+                startTime = new Date();
+                $(".home-box").addClass("hide");
+                $(".quiz-box").removeClass("hide");
+                setQuestion();
+            });
+        }
+    );
 });
 
-function setAvailableSongs() {
-    // get song list from database
-    var settings = {
-        "async": false,
-        "crossDomain": true,
-        "url": "https://soundbites-8ad9.restdb.io/rest/songs",
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        }
-      }
-      
-      $.ajax(settings).done(function (response) {
-        for (let song of response) {
-            if (song.SongArtist == artist)
-            {
-                songList.push(song);
-                availableSongs.push(song);
+$("#play-again").on("click", function () {
+    location.reload();
+});
+
+$("#return").on("click", function () {
+    window.location.href = "select-artist.html";
+});
+
+async function setAvailableSongs() {
+    await Papa.parse("src/csv/" + artist + ".csv", {
+        download: true,
+        header: true,
+        complete: function (results) {
+            for (let i=0; i<results.data.length; i++){
+                songList.push(results.data[i]);
+                allSongs.push(results.data[i]);
             }
-        }
-      });
-}
-
-function getNewSong() {
-    // set available options
-    // allOptions = [];
-    // for (let song of songList) {
-    //     allOptions.push(song);
-    // }
-
-    songList.forEach(song => {
-        allOptions.push(song);
+            songList = shuffleSongs(songList);
+            $(".total-questions").html(songList.length);
+        },
     });
-
-    console.log(allOptions);
-
-    // shuffle songs
-    shuffleSongs(songList);
-    shuffleSongs(availableSongs);
-
-    // set song
-    // get random song
-    const songIndex = availableSongs[Math.floor(Math.random() * availableSongs.length)];
-    currentSong = availableSongs[songIndex];
-
-    // remove currentSong from availableSongs to prevent duplication of question
-    availableSongs.splice(availableSongs.indexOf(songIndex), 1);
-
-    // remove currentSong from allOptions to prevent duplication of options
-    allOptions.splice(allOptions.indexOf(songIndex), 1);
-
-    // set options 
-    // get random options
-    shuffleSongs(allOptions);
-    while (availableOptions.length < 3) {
-        const optionIndex = Math.floor(Math.random() * allOptions.length);
-        while (availableOptions.includes(allOptions[optionIndex])) {
-            optionIndex = Math.floor(Math.random() * allOptions.length);
-        }
-        availableOptions.push(allOptions[optionIndex]);
-    }
-    availableOptions.push(currentSong);
-    shuffleSongs(availableOptions);
-
-    // display options
-    for (let i=0; i<availableOptions.length; i++) {
-        const option = document.createElement("div");
-        console.log(availableOptions[i])
-        option.innerHTML = availableOptions[i].SongName;
-        option.classList.add("option");
-        optionContainer.appendChild(option);
-    }
-    var songName = currentSong.SongName;
-    var songFile = "../src/mp3/" + currentSong.SongArtist + "/" + currentSong.FileName;
-    // play song
-    var audio = new Audio(songFile);
-    audio.play();
-
-    songCounter++;
 }
 
-function next() {
-    if (songCounter == songList.length) {
-        console.log("game over");
-    }
-    else {
-        getNewSong();
-    }
-}
-
-
-async function startQuestion() {
-    var song = songList[Math.floor(Math.random() * songList.length)];
-    while (playedSongs.includes(song)) {
-        song = songList[Math.floor(Math.random() * songList.length)];
-    }
-    playedSongs.push(song);
-    var songName = song.SongName;
-    var songFile = "../src/mp3/" + song.SongArtist + "/" + song.FileName;
+function setQuestion() {
+    let song = songList[songCounter];
+    let songName = song.SongName;
+    let file = "src/mp3/" + artist + "/" + song.FileName;
     // play song
-    var audio = new Audio(songFile);
+    var audio = new Audio(file);
     audio.play();
-    // randomise options
-    var options = [songName];
+    // set options
+    let options = [songName];
     while (options.length < 4) {
-        var randomSong = songList[Math.floor(Math.random() * songList.length)];
+        let randomSong = allSongs[Math.floor(Math.random() * allSongs.length)];
         if (!options.includes(randomSong.SongName)) {
             options.push(randomSong.SongName);
         }
     }
-    // shuffle options
-    shuffleSongs(options);
+    options = shuffleSongs(options);
     // display options
-    displayOptions(options);
+    for (let i=0; i<options.length; i++) {
+        const option = document.createElement("div");
+        option.innerHTML = options[i];
+        option.classList.add("option");
+        optionContainer.append(option);
+    }
+
     // check if answer is correct
     $(".option").on("click", function () {
         audio.pause();
+        $(".option").remove();
         if ($(this).html() == songName) {
-            alert("Correct!");
-            return true;
-        } else {
-            alert("Wrong!");
-            return false;
+            correctAnswers ++;
+        }
+        songCounter ++;
+        if (songCounter < songList.length) {
+            setQuestion();
+        }
+        else {
+            endTime = new Date();
+            $(".quiz-box").addClass("hide");
+            $(".result-box").removeClass("hide");
+            $(".score").html(calculateScore());
+            $(".correct-answers").html(correctAnswers);
+            $(".total-question").html(songList.length);
+            $(".time-taken").html((endTime - startTime) / 1000 + "s");
+            if (!dbDown) {
+                saveScore();
+            }
         }
     });
 }
 
+function getRandomItem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
 function calculateScore() {
-  var score = Math.floor((correctAnswers / totalQuestions) * 100);
+  var timeTaken = (endTime - startTime) / 1000;
+  console.log(timeTaken)
+  var score = Math.floor(((correctAnswers / songList.length) * 10000) / timeTaken);
   return score;
 }
 
 function shuffleSongs(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
+    return arr.sort((a, b) => 0.5 - Math.random());
 }
 
-function displayOptions(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        $("#option" + i).html(arr[i]);
+function saveScore() {
+    var settings = {
+        async: true,
+        crossDomain: true,
+        url: DBLINK,
+        method: "GET",
+        headers: {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache",
+        },
+      };
+    
+      $.ajax(settings).done(function (accounts) {
+        // sort accounts by high score
+        accounts.sort((a, b) => (a.HighScore < b.HighScore ? 1 : -1));
+        for (let i = 0; i < accounts.length; i++) {
+          // update account page
+          if (accounts[i].Username === currentAccount.Username) {
+            if (calculateScore() > accounts[i].HighScore) {
+              accounts[i].HighScore = calculateScore();
+              if (accounts[i].HighestRank > i + 1) {
+                accounts[i].HighestRank = i + 1;
+              }
+            }
+
+            // update database
+            var settings = {
+                async: true,
+                crossDomain: true,
+                url: DBLINK + accounts[i]._id,
+                method: "PUT",
+                headers: {
+                    "content-type": "application/json",
+                    "x-apikey": APIKEY,
+                    "cache-control": "no-cache",
+                },
+                processData: false,
+                data: JSON.stringify(accounts[i]),
+            };
+            $.ajax(settings).done(function (response) {
+                $("#score-update").addClass("show");
+            });
+
+        }
     }
+});
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const APIKEY = "63df95363bc6b255ed0c46aa";
+const DBLINK = "https://soundbites-31d5.restdb.io/rest/accounts";
 var currentAccount;
+
+// check if database can be reached
+try {
+  $ajax({ url: DBLINK });
+} catch (error) {
+  $("#db-down").addClass("show");
+  $("#login-account").attr("disabled", "true");
+  $("#register-account").attr("disabled", "true");
+}
 
 document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
@@ -36,12 +46,160 @@ $(document).ready(function () {
     $(".account-nav").attr("href", "/login.html");
     $(".play-nav").attr("href", "/login.html");
   }
+});
 
+// register new account
+$("#register-account").on("click", function () {
+  // clear old notice
+  $("#login-notice").html("");
+
+  // disable button
+  $("#register-account").attr("disabled", true);
+
+  // get username and password from form
+  var username = $("#username").val();
+  var password = $("#password").val();
+
+  // check if username and password are filled in
+  if (username === "" || password === "") {
+    $("#login-notice").html("Please fill in all fields");
+    $("#register-account").attr("disabled", false);
+  }
+
+  // check if username is already taken
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: DBLINK,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    for (let account of response) {
+      if (username === account.Username) {
+        $("#login-notice").html("Username already taken");
+        $("#register-account").attr("disabled", false);
+        return;
+      }
+    }
+  });
+
+  // create new account
+  var jsondata = {
+    Username: username,
+    Password: password,
+    HighScore: 0,
+    LatestScore: 0,
+  };
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: DBLINK,
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+    processData: false,
+    data: JSON.stringify(jsondata),
+  };
+  $.ajax(settings).done(function (response) {
+    $("#success").html("Account created successfully");
+    // save account to local storage
+    currentAccount = response;
+    localStorage.setItem("currentAccount", JSON.stringify(currentAccount));
+    // redirect to index page
+    window.location.href = "index.html";
+  });
+});
+
+// login account
+$("#login-account").on("click", function () {
+  // clear old notice
+  $("#login-notice").html("");
+
+  // disable button
+  $("#login-account").attr("disabled", true);
+
+  // get username and password from form
+  var username = $("#username").val();
+  var password = $("#password").val();
+
+  // check if username and password are filled in
+  if (username === "" || password === "") {
+    $("#login-notice").html("Please fill in all fields");
+    $("#login-account").attr("disabled", false);
+    return;
+  }
+
+  // check if username and password are correct
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: DBLINK,
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache",
+    },
+  };
+
+  $.ajax(settings).done(function (response) {
+    for (let account of response) {
+      if (username === account.Username) {
+        if (password === account.Password) {
+          currentAccount = account;
+          $("#success").html("Login successful");
+          // save account to local storage
+          localStorage.setItem(
+            "currentAccount",
+            JSON.stringify(currentAccount)
+          );
+          // redirect to account page
+          window.location.href = "account.html";
+          return;
+        } else {
+          $("#login-notice").html("Incorrect Password");
+          $("#login-account").attr("disabled", false);
+          return;
+        }
+      } else {
+        $("#login-notice").html("Username not found");
+        $("#login-account").attr("disabled", false);
+      }
+    }
+  });
+});
+
+// logout account
+$("#logout-account").on("click", function () {
+  localStorage.removeItem("currentAccount");
+  window.location.href = "index.html";
+});
+
+// !- accessing database
+
+// selecting artist
+$(".play-btn").on("click", function () {
+  var artist = $(this).attr("id");
+  // save artist to local storage
+  localStorage.setItem("artist", artist);
+});
+// !- selecting artist
+
+function getDatabase() {
   // update account page and leaderboard
   var settings = {
     async: true,
     crossDomain: true,
-    url: "https://soundbites-8ad9.restdb.io/rest/accounts",
+    url: DBLINK,
     method: "GET",
     headers: {
       "content-type": "application/json",
@@ -51,6 +209,10 @@ $(document).ready(function () {
   };
 
   $.ajax(settings).done(function (accounts) {
+    if (response === 0) {
+      $("#db-down").addClass("show");
+      dbDown = true;
+    }
     // sort accounts by high score
     accounts.sort((a, b) => (a.HighScore < b.HighScore ? 1 : -1));
     for (let i = 0; i < accounts.length; i++) {
@@ -72,150 +234,4 @@ $(document).ready(function () {
       );
     }
   });
-
-  // register new account
-  $("#register-account").on("click", function () {
-    // clear old notice
-    $("#login-notice").html("");
-
-    // disable button
-    $("#register-account").attr("disabled", true);
-
-    // get username and password from form
-    var username = $("#username").val();
-    var password = $("#password").val();
-
-    // check if username and password are filled in
-    if (username === "" || password === "") {
-      $("#login-notice").html("Please fill in all fields");
-      $("#register-account").attr("disabled", false);
-    }
-
-    // check if username is already taken
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url: "https://soundbites-8ad9.restdb.io/rest/accounts",
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache",
-      },
-    };
-
-    $.ajax(settings).done(function (response) {
-      for (let account of response) {
-        if (username === account.Username) {
-          $("#login-notice").html("Username already taken");
-          $("#register-account").attr("disabled", false);
-          return;
-        }
-      }
-    });
-
-    // create new account
-    var jsondata = {
-      Username: username,
-      Password: password,
-      HighScore: 0,
-      LatestScore: 0,
-    };
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url: "https://soundbites-8ad9.restdb.io/rest/accounts",
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache",
-      },
-      processData: false,
-      data: JSON.stringify(jsondata),
-    };
-    $.ajax(settings).done(function (response) {
-      $("#success").html("Account created successfully");
-      // save account to local storage
-      currentAccount = response;
-      localStorage.setItem("currentAccount", JSON.stringify(currentAccount));
-      // redirect to index page
-      window.location.href = "index.html";
-    });
-  });
-
-  // login account
-  $("#login-account").on("click", function () {
-    // clear old notice
-    $("#login-notice").html("");
-
-    // disable button
-    $("#login-account").attr("disabled", true);
-
-    // get username and password from form
-    var username = $("#username").val();
-    var password = $("#password").val();
-
-    // check if username and password are filled in
-    if (username === "" || password === "") {
-      $("#login-notice").html("Please fill in all fields");
-      $("#login-account").attr("disabled", false);
-      return;
-    }
-
-    // check if username and password are correct
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url: "https://soundbites-8ad9.restdb.io/rest/accounts",
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache",
-      },
-    };
-
-    $.ajax(settings).done(function (response) {
-      for (let account of response) {
-        if (username === account.Username) {
-          if (password === account.Password) {
-            currentAccount = account;
-            $("#success").html("Login successful");
-            // save account to local storage
-            localStorage.setItem(
-              "currentAccount",
-              JSON.stringify(currentAccount)
-            );
-            // redirect to account page
-            window.location.href = "account.html";
-            return;
-          } else {
-            $("#login-notice").html("Incorrect Password");
-            $("#login-account").attr("disabled", false);
-            return;
-          }
-        } else {
-          $("#login-notice").html("Username not found");
-          $("#login-account").attr("disabled", false);
-        }
-      }
-    });
-  });
-
-  // logout account
-  $("#logout-account").on("click", function () {
-    localStorage.removeItem("currentAccount");
-    window.location.href = "index.html";
-  });
-
-  // !- accessing database
-
-  // selecting artist
-  $(".play-btn").on("click", function () {
-    var artist = $(this).attr("id");
-    // save artist to local storage
-    localStorage.setItem("artist", artist);
-  });
-  // !- selecting artist
-});
+}
